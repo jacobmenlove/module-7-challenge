816e27e8bea1add35ad78a7a60104ca5babc0cc0
Jacob Menlove <jacobmenlove1@gmail.com>
2024-07-26 18:44:07 -0600

    third commit. added array of questions

--- challenge/Develop/index.js
+++ challenge/Develop/index.js
@@ -2,29 +2,68 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require('path');
-const generateMarkdown = require('./utils/generateMarkdown')
+const generateMarkdown = require('./utils/generateMarkdown');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: 'Input',
-        name: 'Title',
-        message: 'Please name your project.', 
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: input => input ? true : 'Title is required!'
     },
     {
-        type: 'Input',
-        name: 'Description',
-        message: 'Please describe the purpose and function of this project.',
+        type: 'input',
+        name: 'description',
+        message: 'Write a short description of your project:',
+        validate: input => input ? true : 'Description is required!'
     },
     {
-        type: 'checkbox',
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+        default: 'No installation instructions provided.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage examples:',
+        default: 'No usage examples provided.'
+    },
+    {
+        type: 'list',
         name: 'license',
-        message: 'Please select the license applicable to this project.'
-        chocies: 
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+        default: 'None'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Who are the contributors?',
+        default: 'No contributors listed.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What tests are included?',
+        default: 'No tests included.'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: input => input ? true : 'GitHub username is required!'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: input => input ? true : 'Email address is required!'
     }
-
-
 ];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
